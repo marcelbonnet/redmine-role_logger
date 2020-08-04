2432bb0ba42f4c5966ea49ff56efa0a0cf8a6bc0
Marcel Bonnet <marcelbonnet@anatel.gov.br>
2020-08-04 16:38:40 -0300

    remove arquivos da versão 0.1

--- app/models/role_log.rb
+++
@@ -1,7 +0,0 @@
-class RoleLog < ActiveRecord::Base
-	# https://guides.rubyonrails.org/active_record_migrations.html#foreign-keys
-	has_many :role_detail
-	belongs_to :user, :class_name => "Principal"
-	belongs_to :project, :class_name => "Project"
-  	belongs_to :member
-end
